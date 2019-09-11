7a341888583ed3d3046efaaaf695e25209f9cc31
Vbubblery <juncheng.zhou@aizimov.com>
2019-09-11 05:41:15 +0200

    up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,7 +18,82 @@ const App = () => {
   const [names, setNames] = useState<any>(["productA"]);
   return (
     <React.Fragment>
-      <div>123</div>
+      <Layout style={{ minHeight: "100vh" }}>
+        {/* <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={collapsed => {
+            setCollapsed(collapsed);
+          }}
+        >
+          <div className="logo">
+            <span className="logoName">F</span>
+          </div>
+          <Menu
+            theme="dark"
+            defaultSelectedKeys={["dashboard"]}
+            mode="inline"
+            onOpenChange={async open => {
+              if (open.includes("productSheet")) {
+                const { data } = await client.query({
+                  query: allProductsName
+                });
+                setNames(data.products.map((i: any) => i.productName));
+              }
+            }}
+          >
+            <Menu.Item key="dashboard">
+              <Link to="/">
+                <Icon type="line-chart" />
+                <span>DashBoard</span>
+              </Link>
+            </Menu.Item>
+            <SubMenu
+              key="productSheet"
+              title={
+                <span>
+                  <Icon type="form" />
+                  <span>Products</span>
+                </span>
+              }
+            >
+              {names.map((i: string) => (
+                <Menu.Item key={i}>
+                  <Link
+                    to={{
+                      pathname: "/sheet",
+                      search: `?name=${i}`
+                    }}
+                  >
+                    {i}
+                  </Link>
+                </Menu.Item>
+              ))}
+              <Menu.Item key="AddAProductByWebSite">
+                <Link to="/add/data">Add +</Link>
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: "16px 16px" }}>
+            <Switch>
+              <Route exact path="/" render={props => <Dashboard />} />
+              <Route
+                path="/sheet"
+                render={(props: any) => <Sheet {...props} />}
+              />
+              <Route
+                path="/add/data"
+                render={(props: any) => <AddData {...props} />}
+              />
+            </Switch>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Flowlity Design ©2018 Created by Juncheng ZHOU
+          </Footer>
+        </Layout> */}
+      </Layout>
     </React.Fragment>
   );
 };
